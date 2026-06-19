7b7145070683aa6d8c79b08447508c6388d99769
oka05286 <oka.setyarso@gmail.com>
2026-06-19 09:10:13 +0700

    Mengupload isi folder flask_oksa ke github

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+
+# pastikan file ini ada di folder yang sama: model.pkl dan scaler.pkl
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f) # diasumsikan: [Decision Tree, SVC]
+
+with open('scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
+
+model_names = ['Decision tree', 'svc']
+
+@app.route('/')
+def index():
+    return render_template('index.html', model_names=model_names)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = {
+        'Pregnancies': int(request.form['pregnancies']),
+        'Glucose': int(request.form['glucose']),
+        'BloodPressure': int(request.form['blood_pressure']),
+        'SkinThickness': int(request.form['skin_thickness']),
+        'Insulin': int(request.form['insulin']),
+        'BMI': float(request.form['bmi']),
+        'DiabetesPedigreeFunction': float(request.form['diabetes_pedigree_function']),
+        'Age': int(request.form['age'])
+    }
+    
+    df = pd.DataFrame(data, index=[0])
+    X = scaler.transform(df)
+
+    clf = model[model_names.index(request.form['model'])]
+    y = clf.predict(X)
+    
+    # Melengkapi baris yang terpotong
+    prediction = "Diabetic" if int(y[0]) == 1 else "Non-Diabetic"
+
+    # Mengembalikan template dengan membawa data hasil prediksi dan daftar model
+    return render_template('index.html', prediction=prediction, model_names=model_names)
+
+if __name__ == '__main__':
+    app.run(debug=True)
